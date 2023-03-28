36621852aecb73422335e4840a7ad5805ca6560d
OB02CGTH <sornsiri1208@gmail.com>
2023-03-28 15:46:32 +0700

    list month and year

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,12 +1,92 @@
 <template>
   <v-container>
     <a-tabs v-model:activeKey="activeKey">
-      <a-tab-pane key="1" tab="วัน">
+
+      <a-tab-pane key="1" tab="วัน" force-render>
         <a-row justify="space-between">
           <a-col>
             <a-date-picker
               v-model:value="value1"
               :locale="locale"
+              format="DD/MMMM/BBBB"
+              style="width: 20rem" />
+          </a-col>
+          <a-col>
+            <a-row>
+              <a-col class="mr-10">
+                <a-con>
+                  <label>รายรับ </label>
+                  <a-tag color="success">{{ total_quo }}</a-tag>
+                </a-con>
+                <a-con>
+                  <label>| รายจ่าย </label>
+                  <a-tag color="error">{{ total_esti }}</a-tag>
+                </a-con>
+              </a-col>
+              <a-con>
+                <label> กำไร/ขาดทุน </label>
+                <a-tag v-if="total_price >= 0" color="#87d068">{{ total_price }}</a-tag>
+                <a-tag v-else color="#f50">{{ total_price }}</a-tag>
+              </a-con>
+            </a-row>
+          </a-col>
+        </a-row>
+        <div class="relative overflow-x-auto shadow-md mt-4">
+          <table
+            class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead
+              class="text-xs"
+              style="background-color: #1e88e5; color: white">
+              <tr>
+                <th scope="col" class="px-6 py-3">ลำดับ</th>
+                <!-- <th scope="col" class="px-6 py-3">วัน เดือน ปี</th> -->
+                <th scope="col" class="px-6 py-3">รายการ</th>
+                <th scope="col" class="px-6 py-3">รายรับ (บาท)</th>
+                <th scope="col" class="px-6 py-3">รายจ่าย (บาท)</th>
+                <th scope="col" class="px-6 py-3">หมายเหตุ</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                class="table-row-hover"
+                v-for="(item, index) in data_ls"
+                :key="index">
+                <td class="px-6 py-4">{{ index + 1 }}</td>
+                <!-- <td class="px-6 py-4">{{ item.date }}</td> -->
+                <td class="px-6 py-4">
+                  {{
+                    item.objective
+                      ? item.objective
+                      : get_tour_name(item.tour_id)
+                  }}
+                </td>
+                <td class="px-6 py-4">
+                  <a-tag color="green" v-if="item.total_net_price">
+                    {{ item.total_net_price }}
+                  </a-tag>
+                  <div v-else>-</div>
+                </td>
+                <td class="px-6 py-4">
+                  <a-tag color="red" v-if="!item.total_net_price">
+                    {{ item.total_price }}
+                  </a-tag>
+                  <div v-else>-</div>
+                </td>
+                <td class="px-6 py-4"></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </a-tab-pane>
+
+      <a-tab-pane key="2" tab="เดือน" force-render>
+        <a-row justify="space-between">
+          <a-col>
+            <a-date-picker
+              v-model:value="value4"
+              picker="month"
+              :locale="locale"
+              format="MMMM/BBBB"
               style="width: 20rem" />
           </a-col>
           <a-col>
@@ -39,7 +119,7 @@
               style="background-color: #1e88e5; color: white">
               <tr>
                 <th scope="col" class="px-6 py-3">ลำดับ</th>
-                <th scope="col" class="px-6 py-3">วัน เดือน ปี</th>
+                <th scope="col" class="px-6 py-3">วัน</th>
                 <th scope="col" class="px-6 py-3">รายการ</th>
                 <th scope="col" class="px-6 py-3">รายรับ (บาท)</th>
                 <th scope="col" class="px-6 py-3">รายจ่าย (บาท)</th>
@@ -79,13 +159,14 @@
         </div>
       </a-tab-pane>
 
-      <a-tab-pane key="2" tab="เดือน" force-render>
+      <a-tab-pane key="3" tab="ปี" force-render>
         <a-row justify="space-between">
           <a-col>
             <a-date-picker
-              v-model:value="value2"
-              picker="month"
+              v-model:value="value4"
+              picker="year"
               :locale="locale"
+              format="BBBB"
               style="width: 20rem" />
           </a-col>
           <a-col>
@@ -108,11 +189,6 @@
                 <a-tag v-else color="#f50">{{ total_price }}</a-tag>
               </a-con>
             </a-row>
-            <!-- <a-input :value="`฿ ${calculateTotalPrice()}`" style="
-                    width: 20rem;
-                    text-align: center;
-                    border: 1px solid tomato;" 
-            /> -->
           </a-col>
         </a-row>
         <div class="relative overflow-x-auto shadow-md mt-4">
@@ -122,61 +198,9 @@
               class="text-xs"
               style="background-color: #1e88e5; color: white">
               <tr>
-                <th scope="col" class="px-6 py-3">วันที่</th>
-                <th scope="col" class="px-6 py-3">รายรับ (บาท)</th>
-                <th scope="col" class="px-6 py-3">รายจ่าย (บาท)</th>
-                <th scope="col" class="px-6 py-3">หมายเหตุ</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                class="table-row-hover"
-                v-for="(item, index) in data_ls"
-                :key="index">
-                <td class="px-6 py-4">{{ item.date }}</td>
-                <td class="px-6 py-4">
-                  <a-tag color="green" v-if="item.total_net_price">5</a-tag>
-                  <div v-else>-</div>
-                </td>
-                <td class="px-6 py-4">
-                  <a-tag color="red" v-if="!item.total_net_price"> 2</a-tag>
-                  <div v-else>-</div>
-                </td>
-                <td class="px-6 py-4">
-                  <a @click.stop.prevent="activeKey = '1'">ดูรายละเอียด</a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </a-tab-pane>
-
-      <a-tab-pane key="3" tab="ปี">
-        <a-row justify="space-between">
-          <a-col>
-            <a-date-picker
-              v-model:value="value3"
-              picker="year"
-              :locale="locale"
-              style="width: 20rem" />
-          </a-col>
-          <a-col>
-            <!-- <a-input :value="`฿ ${calculateTotalPrice()}`" style="
-                    width: 20rem;
-                    text-align: center;
-                    border: 1px solid tomato;" 
-            /> -->
-            <label>{{ total_price }}</label>
-          </a-col>
-        </a-row>
-        <div class="relative overflow-x-auto shadow-md mt-4">
-          <table
-            class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead
-              class="text-xs"
-              style="background-color: #1e88e5; color: white">
-              <tr>
-                <th scope="col" class="px-6 py-3">เดือนที่</th>
+                <th scope="col" class="px-6 py-3">ลำดับ</th>
+                <th scope="col" class="px-6 py-3">เดือน</th>
+                <th scope="col" class="px-6 py-3">รายการ</th>
                 <th scope="col" class="px-6 py-3">รายรับ (บาท)</th>
                 <th scope="col" class="px-6 py-3">รายจ่าย (บาท)</th>
                 <th scope="col" class="px-6 py-3">หมายเหตุ</th>
@@ -188,22 +212,34 @@
                 v-for="(item, index) in data_ls"
                 :key="index">
                 <td class="px-6 py-4">{{ index + 1 }}</td>
+                <td class="px-6 py-4">{{ item.date }}</td>
                 <td class="px-6 py-4">
-                  <a-tag color="green" v-if="item.total_net_price"></a-tag>
+                  {{
+                    item.objective
+                      ? item.objective
+                      : get_tour_name(item.tour_id)
+                  }}
                 </td>
                 <td class="px-6 py-4">
-                  <a-tag color="red" v-if="!item.total_net_price"> 1</a-tag>
+                  <a-tag color="green" v-if="item.total_net_price">
+                    {{ item.total_net_price }}
+                  </a-tag>
+                  <div v-else>-</div>
                 </td>
                 <td class="px-6 py-4">
-                  <a @click.stop.prevent="activeKey = '2'">ดูรายละเอียด</a>
+                  <a-tag color="red" v-if="!item.total_net_price">
+                    {{ item.total_price }}
+                  </a-tag>
+                  <div v-else>-</div>
                 </td>
+                <td class="px-6 py-4"></td>
               </tr>
             </tbody>
           </table>
         </div>
       </a-tab-pane>
 
-      <a-tab-pane key="4" tab="วันเดือน">
+      <a-tab-pane key="4" tab="วันเดือน" force-render>
         <a-row justify="space-between">
           <a-col>
             <a-date-picker
@@ -317,8 +353,6 @@ export default defineComponent({
   },
   watch: {
     value1() {
-      // console.log(this.value1);
-      // console.log(dayjs(this.value1).format("DD/MM/BBBB"));
       if (this.value1 == null) {
         this.data_ls = this.data_ls2;
       } else {
@@ -331,39 +365,43 @@ export default defineComponent({
       this.data_ls.sort((a: any, b: any) => {
         return a.date < b.date ? -1 : 1;
       });
-      console.log("D" + this.data_ls);
       this.calculateTotalPrice();
     },
     value2() {
-      console.log(
-        dayjs(this.value2).format("DD/MM/BBBB").split("/").slice(1, 2)
-      );
-      this.data_ls2.forEach((x: any) => {
-        console.log(x.date.split("/").slice(1, 2));
+      if (this.value2 == null) {
+        this.data_ls = this.data_ls2;
+      } else {
+      const date = dayjs(this.value2).format("MM/BBBB");
+      this.data_ls = this.data_ls2.filter((x: any) => x.date.includes(date));
+      }
+      this.data_ls.sort((a: any, b: any) => {
+        return a.date < b.date ? -1 : 1;
       });
-
-      this.data_ls = this.data_ls2.filter(
-        (x: any) =>
-          x.date.split("/").slice(1, 2) ==
-          dayjs(this.value2).format("DD/MM/BBBB").split("/").slice(1, 2)
-      );
-      // this.month_price();
-      console.log("M" + this.data_ls);
-      // this.calculateTotalPrice();
-    },
-    value3() {
-      console.log(this.value3);
-      this.data_ls = this.data_ls2.filter(
-        (x: any) =>
-          x.date.split("/")[2] ==
-          dayjs(this.value3).format("DD/MM/BBBB").split("/")[2]
-      );
-      console.log("Y" + this.data_ls);
       this.calculateTotalPrice();
     },
-    value4(newValue) {
-      const date = dayjs(newValue).format("MM/BBBB");
-      this.data_ls = this.data_ls2.filter((x: any) => x.date.includes(date));
+    value3() {
+      if (this.value3 == null) {
+        this.data_ls = this.data_ls2;
+      } else {
+        const date = dayjs(this.value3).format("BBBB");
+        this.data_ls = this.data_ls2.filter((x: any) => x.date.includes(date));
+      }
+      this.data_ls.sort((a: any, b: any) => {
+        return a.date < b.date ? -1 : 1;
+      });
+      this.calculateTotalPrice();
+    },
+    value4() {
+      if (this.value4 == null) {
+        this.data_ls = this.data_ls2;
+      } else {
+        const date = dayjs(this.value4).format("MM/BBBB");
+        this.data_ls = this.data_ls2.filter((x: any) => x.date.includes(date));
+      }
+      this.data_ls.sort((a: any, b: any) => {
+        return a.date < b.date ? -1 : 1;
+      });
+      this.calculateTotalPrice();
     },
   },
   async mounted() {
@@ -406,35 +444,6 @@ export default defineComponent({
     },
     calculateTotalPrice() {
       this.total_price = this.sumTotalNetPrice() - this.sumTotalPrice();
-    },
-
-    month_price() {
-      (this.month = []),
-        this.data_ls.forEach((element: any) => {
-          this.month.push(
-            element.date,
-            this.sumNetDay(element.date),
-            this.sumEstiDay(element.data)
-          );
-        });
-      console.log("MP" + this.month);
-    },
-
-    sumNetDay(date: any) {
-      let sum = 0;
-      this.data_ls.forEach((x: any) => {
-        if (x.date == date && x.total_net_price) {
-          sum += x.total_net_price;
-        }
-      });
-    },
-    sumEstiDay(date: any) {
-      let sum = 0;
-      this.data_ls.forEach((x: any) => {
-        if (x.date == date && x.total_price) {
-          sum += x.total_price;
-        }
-      });
     },
   },
 });
